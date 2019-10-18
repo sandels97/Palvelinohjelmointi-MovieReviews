49ef800bcec5496f2624e5eb7fbb5ec665c690cd
Santtu Hyvärinen <santtu.hyvarinen@kuori.tech>
2019-10-18 09:05:14 +0300

    Bug fixes and small user experience improvements

---
+++ src/main/java/com/santtuhyvarinen/moviereviews/domain/Score.java
@@ -0,0 +1,15 @@
+package com.santtuhyvarinen.moviereviews.domain;
+
+//Used to pass the rating score from moviepage.html to MovieController
+public class Score {
+	public int score;
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
--- src/main/java/com/santtuhyvarinen/moviereviews/web/MovieController.java
+++ src/main/java/com/santtuhyvarinen/moviereviews/web/MovieController.java
@@ -1,34 +1,20 @@
 package com.santtuhyvarinen.moviereviews.web;
 
-import java.io.IOException;
-
-import java.util.Collections;
-
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.santtuhyvarinen.moviereviews.domain.Movie;
 import com.santtuhyvarinen.moviereviews.domain.Review;
+import com.santtuhyvarinen.moviereviews.domain.Score;
 import com.santtuhyvarinen.moviereviews.domain.User;
 import com.santtuhyvarinen.moviereviews.interfaces.GenreRepository;
 import com.santtuhyvarinen.moviereviews.interfaces.MovieRepository;
@@ -74,16 +60,35 @@ public class MovieController {
 	@RequestMapping(value="/movie/{id}")
 	public String moviePage(@PathVariable("id") Long movieId, Model model) {
 
-		Movie movie = movieRepository.findById(movieId).get();;
-
+		//Get currently logged in user to set the rating
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		User user = userRepository.findByUsername(username);
+		
+		Movie movie = movieRepository.findById(movieId).get();
+		
 		//Calculate average score and votes for the movie
 		List<Review> reviews = (List<Review>) reviewRepository.findAll();
 		double averageScore = MovieUtil.calculateAverageScoreFromReviews(movie, reviews);
 		int votes = MovieUtil.calculateVotes(movie, reviews);
 		movie.setAverageScore(averageScore);
 		movie.setVotes(votes);
+		
+		//Score of the movie that user has rated
+		Score score = new Score();
+		
+		//Check if user has already left a review for the movie. If the list is empty, user hasn't reviewed the movie yet
+		List<Review> leftReview = reviewRepository.findByMovieAndUser(movie, user);
+		
+		if(leftReview.size() > 0) {
+			//User has left a review previously - Set the score
+			score.setScore((int)leftReview.get(0).getScore());
+		}
 
+		System.out.println(username + "");
 		model.addAttribute("movie", movie);
+		model.addAttribute("score", score);
+		
 		return "moviepage";
 	}
 	
@@ -107,27 +112,27 @@ public class MovieController {
 	}
 	
 	//Leave a review
-	@RequestMapping("/leaverating/{username}/{movieid}/{rating}") 
-	public String leaveRating(@PathVariable("movieid") Long movieId, @PathVariable("username") String userName, @PathVariable("rating") int rating) {
-		System.out.println(movieRepository.findById(movieId).get().getTitle() + ", " + userName + ", " + rating);
-		
+	@RequestMapping("/leaverating/{username}/{movieid}") 
+	public String leaveRating(@PathVariable("movieid") Long movieId, @PathVariable("username") String userName, Score score) {
+
 		Movie movie = movieRepository.findById(movieId).get();
 		User user = userRepository.findByUsername(userName);
 		
 		//Check if user has already left a review for the movie. If the list is empty, user hasn't reviewed the movie yet
 		List<Review> reviews = reviewRepository.findByMovieAndUser(movie, user);
 		
+		Review review;
+		
 		if(reviews.size() > 0) {
 			//Update previous review
-			Review review = reviews.get(0);
-			Random r = new Random();
-			review.setScore(r.nextInt(5) + 1);
-			reviewRepository.save(review);
+			review = reviews.get(0);
+			review.setScore(score.getScore());
 		} else {
 			//Leave a new review
-			Review review = new Review(movie, user, rating);
-			reviewRepository.save(review);
+			review = new Review(movie, user, score.getScore());
 		}
+		
+		reviewRepository.save(review);
 		
 		return "redirect:/movie/" + movieId;
 	}
