0f17e6ff815a867ad38af64ccf43b24d2b97156d
Santtu Hyvärinen <santtu.hyvarinen@kuori.tech>
2019-10-27 11:08:59 +0200

    Integrated mysql database and other improvements

--- src/main/java/com/santtuhyvarinen/moviereviews/MovieReviewsApplication.java
+++ src/main/java/com/santtuhyvarinen/moviereviews/MovieReviewsApplication.java
@@ -28,22 +28,31 @@ public class MovieReviewsApplication {
 			GenreRepository genreRepository, UserRepository userRepository, ReviewRepository reviewRepository) {
 		return (args) -> {
 			
-			//For testing with the H2 database
+			//Add some genres to the database, if there aren't any rows in the genre table
+			if(genreRepository.count() == 0) {
+				genreRepository.save(new Genre("Action"));
+				genreRepository.save(new Genre("Horror"));
+				genreRepository.save(new Genre("Drama"));
+				genreRepository.save(new Genre("Fantasy"));
+				
+				genreRepository.save(new Genre("Romance"));
+				genreRepository.save(new Genre("Comedy"));
+				genreRepository.save(new Genre("Thriller"));
+				genreRepository.save(new Genre("Western"));
+				genreRepository.save(new Genre("Scifi"));
+			}
 			
-			/*Genre action = genreRepository.save(new Genre("Action"));
-			Genre horror = genreRepository.save(new Genre("Horror"));
-			Genre drama = genreRepository.save(new Genre("Drama"));
-			Genre fantasy = genreRepository.save(new Genre("Fantasy"));
-
-			Movie heat = movieRepository.save(new Movie("Heat", 1995, action));
+			//For testing with the H2 database
+			/*Movie heat = movieRepository.save(new Movie("Heat", 1995, action));
 			Movie halloween = movieRepository.save(new Movie("Halloween", 1978, horror));
 			Movie cuckoo = movieRepository.save(new Movie("One Flew Over the Cuckoo's Nest", 1975, drama));
 			Movie lotr = movieRepository.save(new Movie("The Lord of the Rings: The Fellowship of the Ring", 2001, fantasy));*/
+			//User user = userRepository.save(new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6","USER"));
 			
-			User user = userRepository.save(new User("user","$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6","USER"));
-			
-			//Add admin account
-			userRepository.save(new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","ADMIN"));
+			//Add admin account if one does not exist yet
+			if(userRepository.findByUsername("admin") == null) {
+				userRepository.save(new User("admin","$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C","ADMIN"));
+			}
 		};
 	}
 }
